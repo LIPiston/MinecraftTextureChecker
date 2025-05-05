702d6ce8eed0b258edc0cf5100a8beb275c49549
LIPiston <wps1910093789@gmail.com>
2025-05-05 18:11:25 +0800

    在后端和前端启动脚本中添加 npm install 命令，以确保依赖项在启动服务之前正确安装

--- run-backend.bat
+++ run-backend.bat
@@ -2,5 +2,6 @@
 REM 切换到项目根目录下的 server 文件夹
 cd /d "%~dp0server"
 REM 启动后端服务
+npm i
 npm start
 pause 
--- run-frontend.bat
+++ run-frontend.bat
@@ -2,5 +2,6 @@
 REM 切换到项目根目录下的 client 文件夹
 cd /d "%~dp0client"
 REM 启动前端开发服务器
+npm i
 npm run dev
 pause 
